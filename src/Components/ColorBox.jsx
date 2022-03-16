f1850e4c032e7bc4653766d562ca1fe36803f149
Faruk Cakir <farukckr55@gmail.com>
2022-03-16 20:25:13 +0300

    some fixes

--- src/Components/ColorBox.jsx
+++ src/Components/ColorBox.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../CSS/ColorBox.css"
+import { CopyToClipboard } from "react-copy-to-clipboard"
 
 function ColorBox({ background, name }) {
+  const [coppied, setCoppied] = useState(false)
+
+
   return (
-    <div style={{background: background}} className='ColorBox'>
-        <div className="container">
-          <div className="content">
-            <span>{name}</span>
+      <CopyToClipboard text={background} onCopy={() => {
+        setCoppied(true)
+        setTimeout(() => setCoppied(false), 1900)
+      }}>
+        <div style={{ background: background }} className='ColorBox'>
+          <div style={{ background: background }} className={`overlay ${coppied && "display"}`} />
+          <div className={`message ${coppied && "display"}`}>
+            <h1>Copied!</h1>
+            <p>{background}</p>
           </div>
-          <button className='copy-btn'>COPY</button>
+          <div className="container">
+            <div className="content">
+              <span>{name}</span>
+            </div>
+            <button className='copy-btn'>COPY</button>
+          </div>
+          <span className='more-btn'>MORE</span>
         </div>
-        <span className='more-btn'>MORE</span>
-    </div>
+      </CopyToClipboard>
+
   )
 }
 
