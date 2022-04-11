4243d8f6a68b5a3c06bacf501610aa1d5885af1c
Faruk Cakir <farukckr55@gmail.com>
2022-04-11 21:44:16 +0300

    some fixes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Router>
+  <HashRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Router>
+  </HashRouter>
   ,
   document.getElementById('root')
 );
